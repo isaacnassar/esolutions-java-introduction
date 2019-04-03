823a71585047ad0177eaa92e396f93235fa5e233
Alan Forneron <alan.forneron@claro.com.ar>
2019-04-03 17:14:01 -0300

    se sube ejercicio numero 6 completo

--- src/com/esolutions/trainings/java/nivelacion/ex6/JavaCollections.java
+++ src/com/esolutions/trainings/java/nivelacion/ex6/JavaCollections.java
@@ -1,18 +1,42 @@
 package com.esolutions.trainings.java.nivelacion.ex6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class JavaCollections {
     public int[] distinct(int[] arr) {
    //b. Obtener un array que solo contenga los valores no repetidos. 
+    	ArrayList<Integer> distintos = new ArrayList<Integer>();
     	
-    	
-        return arr;
+    	for (int i = 0; i < arr.length; i++) {
+    		
+				if (!distintos.contains(arr[i])) {
+					distintos.add(arr[i]);		
+			}
+			
+		}
+    	int [] array = new int[distintos.size()];
+    	for (int i = 0; i < distintos.size(); i++) {
+			array[i]=distintos.get(i);
+	
+		}
+        return array;
     }
 
+    
+    
     public int[] sorted(int[] arr) {
     //a.Ordenar todos los datos del array.
-        return arr;
+    	int [] a = arr;
+    	Arrays.sort(a);
+    	
+    return a;
     }
     
     /**
@@ -32,7 +56,20 @@ public class JavaCollections {
     public Map<Integer, Integer> countByNumber(int[] arr) {
 //c. Obtener un mapa cuyas claves sean cada uno de los valores no repetidos del array original 
 //y el valor la cantidad de ocurrencias de ese número en el array original.
+     int [] a = arr;
+    //	Arrays.sort(a);
+    	int v=1;
+    	Map<Integer, Integer> mapa1 = new HashMap<Integer, Integer>();
 
-        return null;
+    	for (int i = 0; i < a.length; i++) {
+			if (!mapa1.containsKey(a[i])) {
+				mapa1.put(a[i], v);
+			}
+			else {
+				mapa1.put(a[i], mapa1.get(a[i])+1);
+			}
+		}
+    	
+        return mapa1;
     }
 }
