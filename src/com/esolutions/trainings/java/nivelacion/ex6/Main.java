01bd3a536ce590d637d1d53b3642adae4f8be0d6
Solido <emanuel.ponce@esolutions.com.ar>
2019-04-03 17:35:59 -0300

    se resuelve ejercicio 2 y 6

--- src/com/esolutions/trainings/java/nivelacion/ex6/Main.java
+++ src/com/esolutions/trainings/java/nivelacion/ex6/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         Console.printHeader(6);
-        Console.printHeader(6);
 
         final int[] arr = new int[]{10, 20, 40, 123, 90, 88, 122, 44, 100, 121, 99, 99, 123, 0, 90, 1, 90, 2, 90};
 
